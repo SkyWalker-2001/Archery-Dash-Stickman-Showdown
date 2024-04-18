51f57c53fac9274bda2bd00de6119c015180aec1
SkyWalker-2001 <126860277+SkyWalker-2001@users.noreply.github.com>
2024-04-18 09:58:31 +0530

    Archer game archive

--- Assets/Scripts/Managers/UI_Manager.cs
+++ Assets/Scripts/Managers/UI_Manager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class UI_Manager : MonoBehaviour
 {
+    private void Awake() {
+        Application.targetFrameRate = 144;
+    }
+
     public void Attack_Button()
     {
         Arrow_Spawn.arrow_Spawn_Instance.Arrow_Spawn_FN();
